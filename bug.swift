e08ff8132ecf55aee2c155d38901a132bf48d098
BugHunter-X <askzenai@gmail.com>
2024-12-14 02:28:05 +0100

    Add bug.swift file

---
+++ bug.swift
@@ -0,0 +1,11 @@
+func myFunction(param: String) -> String {
+    guard let url = URL(string: param) else {
+        return "Invalid URL"
+    }
+    do {
+        let contents = try String(contentsOf: url)
+        return contents
+    } catch {
+        return "Error reading URL contents"
+    }
+}
